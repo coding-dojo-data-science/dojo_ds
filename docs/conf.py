0710bca64517c2a739e2b56d96c5b552c6024a25
James M. Irving, Ph.D <james.irving.phd@gmail.com>
2024-01-23 18:55:35 -0500

    Added rtd theme for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ import dojo_ds
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
               'sphinx.ext.apidoc','sphinx.ext.napoleon',
-	      'sphinx.ext.autosummary']
+	      'sphinx.ext.autosummary', 'sphinx_rtd_theme']
 
 ## Napoleon Docstring settings
 napoleon_numpy_docstring = True
