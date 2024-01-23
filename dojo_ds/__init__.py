bc25e4a17f80a60420651b714bf2c79e943e163c
James M. Irving, Ph.D <james.irving.phd@gmail.com>
2024-01-23 16:13:23 -0500

    Bump version: 1.0.1 → 1.0.2

--- dojo_ds/__init__.py
+++ dojo_ds/__init__.py
@@ -4,7 +4,7 @@ __author__ = James Irving, Brenda Hungerford
 """
 __author__ = """James Irving"""
 __email__ = 'james.irving.phd@gmail.com'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 from . import data_enrichment as data
 from . import eda 
 from . import evaluate
