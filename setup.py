482a6b920dbbe83f7639fbd9410d2c5bd3c80a04
James M. Irving, Ph.D. <james.irving.phd@gmail.com>
2024-02-22 21:16:06 -0500

    Bump version: 1.0.10 → 1.0.11

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/coding-dojo-data-science/dojo_ds',
-    version='1.0.10',
+    version='1.0.11',
     zip_safe=False,
 )
