1a44b7edc4a8065032ea273712f664a266cca246
James M. Irving, Ph.D. <james.irving.phd@gmail.com>
2024-08-21 16:15:37 -0400

    Added python 3.11 and 3.12

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
     ],
     description="Code from Coding Dojo's Online Part-Time Data Science boot camp",
     entry_points={
